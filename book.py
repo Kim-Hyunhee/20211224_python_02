d42b3f1f3b3bfc6b7fb3f7036971deceb2353b21
김현희 <tta_bi@naver.com>
2021-12-24 15:55:05 +0900

    book 클래스 기능 추가 - 정보 출력하기
    rent_user 변수가 실제로 있을 때 / 없을 때를 구별해서 코딩

--- book.py
+++ book.py
@@ -3,3 +3,15 @@ class Book:
         self.title = title
         self.rent_fee = rent_fee
         self.limit_age = limit_age
+        self.rent_user = None
+        
+    def print_book_info(self):
+        print('===== 책 정보 출력 =====')
+        print(f'책 이름 : {self.title}')
+        print(f'대여료 : {self.rent_fee} P')
+        print(f'연령 제한 : {self.limit_age} 세')
+        
+        if self.rent_user :
+            pass
+        else:
+            print('대여해 간 사람이 없습니다.')
